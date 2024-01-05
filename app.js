69c2b095c3647130034a13d69c5c5f4937c6bb71
CrazyTapok <masyanya0772@gmail.com>
2024-01-06 01:02:29 +0300

    refactor: add media alternatives in HTML, JS according to rule 1.2 in html-and-css-exten.md

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ var createNewTaskElement = function (taskString) {
   var deleteButton = document.createElement("button");//delete button
   var deleteButtonImg = document.createElement("img");//delete button image
   deleteButtonImg.className = "btn__img_delete";
+  deleteButtonImg.alt = "Delete btn image";
 
   label.innerText = taskString;
   label.className = "item item__label";
